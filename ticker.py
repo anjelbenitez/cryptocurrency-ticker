a910c93cfd0c009a49b9b113f177035a98f8b380
Anjelica Benitez <55768082+anjelbenitez@users.noreply.github.com>
2021-01-16 02:51:53 -0800

    Fixed alignment issues

--- ticker.py
+++ ticker.py
@@ -87,10 +87,10 @@ class Ticker(Frame):
         change_width = sum(
             [font_change.CharacterWidth(ord(c)) for c in asset['change_24h']]
         )
-        change_x = 66 - change_width
+        change_x = 65 - change_width
 
         # Get colors
-        main_color = graphics.Color(168, 149, 105)
+        main_color = graphics.Color(255,255,255)
         change_color = (
             graphics.Color(194, 24, 7)
             if asset['change_24h'].startswith('-')
