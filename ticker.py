a992a0a5f829c5850b90ffb8cb0f5bd6a6410ba6
Anjelica Benitez <55768082+anjelbenitez@users.noreply.github.com>
2021-01-16 01:49:29 -0800

    Testing right align

--- ticker.py
+++ ticker.py
@@ -87,7 +87,7 @@ class Ticker(Frame):
         change_width = sum(
             [font_change.CharacterWidth(ord(c)) for c in asset['change_24h']]
         )
-        change_x = 62 - change_width
+        change_x = 63 - change_width
 
         # Get colors
         main_color = graphics.Color(255, 255, 255)
