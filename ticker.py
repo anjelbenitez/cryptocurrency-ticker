83625d8106d43ef578f9f2f41048516673a2d16f
Anjelica Benitez <55768082+anjelbenitez@users.noreply.github.com>
2021-01-16 02:21:19 -0800

    Update graphics and text align

--- ticker.py
+++ ticker.py
@@ -87,10 +87,10 @@ class Ticker(Frame):
         change_width = sum(
             [font_change.CharacterWidth(ord(c)) for c in asset['change_24h']]
         )
-        change_x = 63 - change_width
+        change_x = 67 - change_width
 
         # Get colors
-        main_color = graphics.Color(255, 255, 255)
+        main_color = graphics.Color(196, 162, 83)
         change_color = (
             graphics.Color(194, 24, 7)
             if asset['change_24h'].startswith('-')
